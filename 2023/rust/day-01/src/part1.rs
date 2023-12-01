e1eae7f83c37e46457f4f0e7757e20baf9fe19fd
jilquero <szczygielski.mateusz@wp.pl>
2023-12-02 00:57:40 +0100

    add day-01

---
+++ 2023/rust/day-01/src/part1.rs
@@ -0,0 +1,31 @@
+use crate::custom_error::AocError;
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let output = input
+        .lines()
+        .map(|line| {
+            let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+            let first = digits.next().expect("should be number");
+            let last = digits.last().unwrap_or(first);
+            first * 10 + last
+        })
+        .sum::<u32>();
+
+    Ok(output.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!("142", process(input)?);
+        Ok(())
+    }
+}
---
+++ 2023/rust/day-01/src/part2.rs
@@ -0,0 +1,45 @@
+use crate::custom_error::AocError;
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let output = input
+        .lines()
+        .map(|line| {
+            line.replace("one", "o1e")
+                .replace("two", "t2o")
+                .replace("three", "t3e")
+                .replace("four", "f4r")
+                .replace("five", "f5e")
+                .replace("six", "s6x")
+                .replace("seven", "s7n")
+                .replace("eight", "e8t")
+                .replace("nine", "n9e")
+        })
+        .map(|line| {
+            let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+            let first = digits.next().expect("should be number");
+            let last = digits.last().unwrap_or(first);
+            first * 10 + last
+        })
+        .sum::<u32>();
+
+    Ok(output.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!("281", process(input)?);
+        Ok(())
+    }
+}
